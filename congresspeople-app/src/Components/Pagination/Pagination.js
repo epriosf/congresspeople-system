f0ecc0c5d48b2177bbff13a0421df72b7915704e
Erick Rios <paulrios@outlook.es>
2023-01-17 03:17:56 -0500

    CP-4 Adding a client side pagination for congressPeople list

--- congresspeople-app/src/Components/Pagination/Pagination.js
+++ congresspeople-app/src/Components/Pagination/Pagination.js
@@ -1,8 +1,18 @@
 import {Paginator} from "primereact/paginator";
-
-const Pagination = () => {
+import {useState} from "react";
+/* eslint react/prop-types: 0 */
+const Pagination = ({first, rows, onLoadPagination, congressPeopleNumber}) => {
+    const [basicFirst, setBasicFirst] = useState(first || 0);
+    const [basicRows, setBasicRows] = useState(rows || 12);
+    const onBasicPageChange = (event) => {
+        setBasicFirst(event.first);
+        setBasicRows(event.rows);
+        onLoadPagination(event);
+    }
     return (
-        <Paginator/>
+        <Paginator first={basicFirst} rows={basicRows} totalRecords={congressPeopleNumber}
+                   rowsPerPageOptions={[12, 24, 36]}
+                   onPageChange={onBasicPageChange}></Paginator>
     );
 
 }
